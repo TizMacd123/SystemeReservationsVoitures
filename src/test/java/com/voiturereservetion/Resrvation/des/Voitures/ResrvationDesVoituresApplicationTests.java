52dba34657ec4606a5d530a1b8d0d4cb8797c5d2
TizMacd123 <hamdaouihamza541@gmail.com>
2025-05-13 12:13:47 +0100

    fordt commit

---
+++ src/test/java/com/voiturereservetion/Resrvation/des/Voitures/ResrvationDesVoituresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voiturereservetion.Resrvation.des.Voitures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResrvationDesVoituresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
